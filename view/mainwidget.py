c014089cfbbb4f66177bf33310e16155a68fff2e
duoyi-window <1323242382@qq.com>
2018-07-24 14:45:37 +0800

    正则

--- view/mainwidget.py
+++ view/mainwidget.py
@@ -231,7 +231,7 @@ class CMainWidget(QtWidgets.QMainWindow, mainwidget_ui.Ui_MainWindow):
             return False
         fangan = self.comboBox.currentText()
         for line in self.m_FilterInfo.get(fangan, ""):
-            if sInfo.find(line) != -1:
+            if re.search(line, sInfo):
                 return True
         return False
 
